99f9710bc0b1f32f64db2d4ba1bab4a6c17b134f
noisygerman <mwesterholz@gmail.com>
2018-02-16 10:45:02 -0800

    <tools>/renderMermaidDiagrams/prepareMermaidRenderData[.test].js

--- spec/tools/renderMermaidDiagrams/prepareMermaidRenderData.test.js
+++ spec/tools/renderMermaidDiagrams/prepareMermaidRenderData.test.js
@@ -17,27 +17,44 @@ describe( 'The <tools>/renderMermaidDiagrams/prepareMermaidRenderData function',
   const DiagramTextBlock1 = `\`\`\`mermaid\n${ DiagramText1 }\`\`\``;
   const DiagramTextBlock2 = `\`\`\`mermaid\n${ DiagramText2 }\`\`\``;
   
+  const outputInfoDiagram1 = Object.freeze( {
+    diagramTarget: 'where/to/copy/rendered/markdown/file/media/title-1.svg',
+    imageLinkText: '![alt Title1](where/to/copy/rendered/markdown/file/media/title-1.svg)',
+    text:          'graph TD;\n  A-->B;\n  A-->C;\n  B-->D;\n',
+    title:         'Title1',
+    rangesInFile:  [ { startIndex: 0, length: 62 }, { startIndex: 119, length: 51  } ] 
+  } );
 
-  it( 'should generate an output path with the kebab-case version of the tile', ()=>{
+
+  const outputInfoDiagram2 = Object.freeze( {
+    diagramTarget: 'where/to/copy/rendered/markdown/file/media/title-2.svg',
+    imageLinkText: '![alt Title2](where/to/copy/rendered/markdown/file/media/title-2.svg)',
+    text:          'graph TD;\n  A-->B;\n  B-->C;\n',
+    title:         'Title2',
+    rangesInFile:  [ { startIndex: 64, length: 53 } ] 
+  } );
+
+  it( 'should generate an array of render data sorted by injection start indices', ()=>{
 
     const text = `${ TitleTextBlock1 }${ Padding }${ DiagramTextBlock1 }${
       Padding }${ TitleTextBlock2 }${ Padding }${ DiagramTextBlock2 }${
       Padding }${ DiagramTextBlock1 }${ Padding }`;
 
     const expected = [
-      {
-        diagramTarget: 'where/to/copy/rendered/markdown/file/media/title-1.svg',
-        imageLinkText: '![alt Title1](where/to/copy/rendered/markdown/file/media/title-1.svg)',
-        text:          'graph TD;\n  A-->B;\n  A-->C;\n  B-->D;\n',
-        title:         'Title1',
-        rangesInFile:  [ { startIndex: 0, length: 62 }, { startIndex: 119, length: 51  } ]
+      { 
+        startIndex: 0,
+        length:     62,
+        output:     outputInfoDiagram1
+      }, 
+      { 
+        startIndex: 64,
+        length:     53,
+        output:     outputInfoDiagram2
       },
-      {
-        diagramTarget: 'where/to/copy/rendered/markdown/file/media/title-2.svg',
-        imageLinkText: '![alt Title2](where/to/copy/rendered/markdown/file/media/title-2.svg)',
-        text:          'graph TD;\n  A-->B;\n  B-->C;\n',
-        title:         'Title2',
-        rangesInFile:  [ { startIndex: 64, length: 53 } ]
+      { 
+        startIndex: 119,
+        length:     51,
+        output:     outputInfoDiagram1
       }
     ];
 
--- tools/renderMermaidDiagrams/prepareMermaidRenderData.js
+++ tools/renderMermaidDiagrams/prepareMermaidRenderData.js
@@ -1,11 +1,14 @@
 const findUniqueMermaidDiagramTextBlocks = require( './findUniqueMermaidDiagramTextBlocks' );
 const generateDiagramImageOutputPath     = require( './generateDiagramImageOutputPath' );
+const mapRenderDataToFileRanges          = require( './mapRenderDataToFileRanges' );
 
 function prepareMermaidRenderData( { target, text } ){
 
   return findUniqueMermaidDiagramTextBlocks( text )
     .map( dtb=>Object.assign( { rangesInFile: dtb.rangesInFile, diagramTarget: generateDiagramImageOutputPath( target, dtb ) }, dtb ) ) // eslint-disable-line max-len
-    .map( dtb=>Object.assign( { imageLinkText: `![alt ${ dtb.title || dtb.diagramTarget }](${ dtb.diagramTarget })` }, dtb ) ); // eslint-disable-line max-len
+    .map( dtb=>Object.assign( { imageLinkText: `![alt ${ dtb.title || dtb.diagramTarget }](${ dtb.diagramTarget })` }, dtb ) ) // eslint-disable-line max-len
+    .reduce( mapRenderDataToFileRanges, [] )
+    .sort( ( lhs, rhs )=>lhs.startIndex - rhs.startIndex );
 
 }
 
