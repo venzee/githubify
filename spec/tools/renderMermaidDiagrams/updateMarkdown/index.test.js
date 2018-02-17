057ff8d9e26dea6c02a913cb04a74946492d2107
noisygerman <mwesterholz@gmail.com>
2018-02-17 00:34:58 -0800

    <tools>/renderMermaidDiagrams/index[.test].js

---
+++ spec/tools/renderMermaidDiagrams/updateMarkdown/index.test.js
@@ -0,0 +1,52 @@
+import updateMarkdown from '<tools>/renderMermaidDiagrams/updateMarkdown';
+
+describe( 'The <tools>/renderMermaidDiagrams/updateMarkdown function', ()=>{
+
+  it( 'should replace all Mermaid diagram text blocks with image links', ()=>{
+
+    const Title1          = 'Title1';
+    const Title2          = 'Title2';
+    const TitleTextBlock1 = `## ${ Title1 }`;
+    const TitleTextBlock2 = `## ${ Title2 }`;
+    const Padding         = '\n\n';
+
+    const DiagramText1 = 'graph TD;\n  A-->B;\n  A-->C;\n  B-->D;\n';
+    const DiagramText2 = 'graph TD;\n  A-->B;\n  B-->C;\n';
+
+    const DiagramTextBlock1 = `\`\`\`mermaid\n${ DiagramText1 }\`\`\``;
+    const DiagramTextBlock2 = `\`\`\`mermaid\n${ DiagramText2 }\`\`\``;
+  
+    const outputInfoDiagram1 = Object.freeze( {
+      diagramTarget: 'where/to/copy/rendered/markdown/file/media/title-1.svg',
+      imageLinkText: '![alt Title1](where/to/copy/rendered/markdown/file/media/title-1.svg)',
+      text:          'graph TD;\n  A-->B;\n  A-->C;\n  B-->D;\n',
+      title:         'Title1',
+      rangesInFile:  [ { startIndex: 0, length: 62 }, { startIndex: 119, length: 51  } ] 
+    } );
+
+
+    const outputInfoDiagram2 = Object.freeze( {
+      diagramTarget: 'where/to/copy/rendered/markdown/file/media/title-2.svg',
+      imageLinkText: '![alt Title2](where/to/copy/rendered/markdown/file/media/title-2.svg)',
+      text:          'graph TD;\n  A-->B;\n  B-->C;\n',
+      title:         'Title2',
+      rangesInFile:  [ { startIndex: 64, length: 53 } ] 
+    } );
+  
+
+    const text = `${ TitleTextBlock1 }${ Padding }${ DiagramTextBlock1 }${
+      Padding }${ TitleTextBlock2 }${ Padding }${ DiagramTextBlock2 }${
+      Padding }${ DiagramTextBlock1 }`;
+
+    const expected = `![alt Title1](where/to/copy/rendered/markdown/file/media/title-1.svg)${
+      Padding }![alt Title2](where/to/copy/rendered/markdown/file/media/title-2.svg)${
+      Padding }![alt Title1](where/to/copy/rendered/markdown/file/media/title-1.svg)`;
+
+    const actual = updateMarkdown( text )( [ outputInfoDiagram1, outputInfoDiagram2 ] );
+  
+    expect( actual )
+      .to.equal( expected );
+
+  } );
+
+} );
---
+++ tools/renderMermaidDiagrams/index.js
@@ -0,0 +1,109 @@
+const just     = require( 'object-streaming-tools/lib/just' ); 
+const toArray  = require( 'object-streaming-tools/lib/toArray' ); 
+const apply    = require( 'object-streaming-tools/lib/apply' ); 
+const asyncify = require( 'async/asyncify' );
+const fs       = require( 'fs-extra' );
+const path     = require( 'path' );
+const nrc      = require( 'node-run-cmd' );
+
+const prepareMermaidRenderData = require( './prepareMermaidRenderData' );
+const updateMarkdown           = require( './updateMarkdown' );
+
+const TempDirPrefix = 'renderMermaidDiagrams'; 
+
+function renderMermaidDiagrams( { target, text } , onRendered ){
+
+  just( { target, text } )
+    .on( 'error', onRendered )
+    .pipe( apply( asyncify( prepareMermaidRenderData ) ) )
+    .on( 'error', onRendered )
+    .pipe( apply( createTempDir ) )
+    .on( 'error', onRendered )
+    .pipe( apply( renderDiagrams ) )
+    .on( 'error', onRendered )
+    .pipe( apply( deleteTempDir ) )
+    .pipe( apply( asyncify( updateMarkdown( text ) ) ) )
+    .on( 'error', onRendered )
+    .on( 'data', onRendered.bind( null, null ) );
+
+}
+
+module.exports = renderMermaidDiagrams;
+
+function createTempDir( renderData, next ){
+
+  fs.mkdtemp( TempDirPrefix, ( err, tempDir )=>{
+
+    if( err ) return next( err );
+    next( null, { renderData, tempDir } );
+
+  } );
+
+}
+
+function deleteTempDir( { renderData, tempDir }, next ){
+
+  fs.remove( tempDir, err=>{
+
+    if( err ) return next( err );
+    next( null, renderData );
+
+  } );
+
+}
+
+function renderDiagrams( { renderData, tempDir }, onDiagramsRendered ){
+
+  just( ...renderData )
+    .on( 'error', onDiagramsRendered )
+    .pipe( apply( asyncify( appendTempTarget.bind( null, tempDir ) ) ) )
+    .on( 'error', onDiagramsRendered )
+    .pipe( apply( writeTempFile ) )
+    .on( 'error', onDiagramsRendered )
+    .pipe( apply( renderDiagram ) )
+    .on( 'error', onDiagramsRendered )
+    .pipe( toArray() )
+    .on( 'data', renderData=>onDiagramsRendered( null, { renderData, tempDir } ) )
+    .on( 'error', onDiagramsRendered );
+
+}
+
+function appendTempTarget( tempDir, renderData ){
+
+  return Object.assign(
+    {},
+    renderData,
+    { tempTarget: path.join( tempDir, renderData.diagramTarget ) }
+  );
+
+}
+
+function writeTempFile( renderData, onTempFileWritten ){
+
+  fs.writeFile( renderData.tempTarget, renderData.text, err=>{
+
+    if( err ) return onTempFileWritten( err );
+    onTempFileWritten( null, renderData );
+    
+  } );
+
+}
+
+function renderDiagram( renderData, onDiagramRendered ){
+
+  const errors  = [];
+  const onError =  errors.push.bind( errors );
+
+  function onDone( code ){
+
+    if( code ) return onDiagramRendered( new Error( errors.join( '' ) ) );
+    onDiagramRendered( null, renderData );
+
+  }
+
+  nrc.run(
+    `./node_modules/.bin/mmdc -i ${ renderData.tempTarget } -o ${ renderData.diagramTarget }`,
+    { onError, onDone }
+  );
+
+}
